cda8735c866424c72e1c9e95f63a8d005471fab8
563319 <563319@escg.ac.uk>
2026-02-10 18:24:26 +0000

    update11

--- Assets/scripts/enemySpawn.cs
+++ Assets/scripts/enemySpawn.cs
@@ -5,33 +5,23 @@ public class enemySpawn : MonoBehaviour
 {
     public GameObject enemy;
     public float spawnTimer = 25;
-    float counter2 = 100;
+    
     public float waveCounter = 1;
     bool canSpawn = true;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        if (waveCounter % 3 == 0)
-        {
-            canSpawn = false;
-            counter2 -= Time.deltaTime;
-            if (counter2 == 0)
-            {
-                canSpawn = true;
-            }
-
-
-        }
+        
         spawnTimer -= Time.deltaTime;
         if (spawnTimer <= 0 && canSpawn == true)
         {
-            // Instantiate the bullet at the position and rotation of the player
+            
             GameObject clone;
             print("spawner:" + gameObject.name);
             clone = Instantiate(enemy, transform.position, transform.rotation);
@@ -39,7 +29,7 @@ public class enemySpawn : MonoBehaviour
 
 
 
-            spawnTimer = 10;
+            spawnTimer = 25;
         }
 
     }
